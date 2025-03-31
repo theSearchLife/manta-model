382d2e8d974baa0bafbac3718b5d9ee080ec5cf6
massimo <massidl.pd@gmail.com>
2025-03-31 09:03:13 +0200

    add predict_manta_postprocess.py

---
+++ predict_manta_postprocess.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import shutil
+import argparse
+
+
+
+
+if __name__=='__main__':
+   #parse parameters
+   parser = argparse.ArgumentParser()
+   
+   parser.add_argument("--folder_images", type=str,required=True)
+   parser.add_argument("--folder_out", type=str,default='')
+   parser.add_argument("--filename_csv", type=str,required=True)
+   
+   
+   args = parser.parse_args()
+   
+   folder=args.folder_images
+   filename_csv=args.filename_csv
+   folder_out=args.folder_out
+   if len(folder_out)==0:
+      folder_out=folder+'_out'
+   
+   folder_out_manta=folder_out+'/'+'manta'
+   folder_out_nonmanta=folder_out+'/'+'non_manta'
+   os.makedirs(folder_out_manta,exist_ok=True)
+   os.makedirs(folder_out_nonmanta,exist_ok=True)
+   df = pd.read_csv(filename_csv)
+   for ct in range(len(df)):
+      filename= df['filename'].iloc[ct]
+      prediction= df['prediction'].iloc[ct]
+      #print( filename,prediction)
+      if prediction == 'manta':
+         path_out=folder_out_manta+'/'+filename
+      else:
+         path_out=folder_out_nonmanta+'/'+filename
+      src=folder+'/'+filename
+      dst=path_out
+      #print(src,dst)
+      shutil.copyfile(src,dst)
+
+   print('done', folder_out)
